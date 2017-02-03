fda0ffd3a8d1d3c615efd033963fb57f26e0a683
李卫友 <wei-you.li@utsoft.cn>
2017-02-03 09:21:07 +0800

    Initial Commit

---
+++ Demo3DTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo3DTouch
+//
+//  Created by yons on 17/2/3.
+//  Copyright © 2017年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo3DTouch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Demo3DTouch
+//
+//  Created by yons on 17/2/3.
+//  Copyright © 2017年 yons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
